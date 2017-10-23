dba5bd4fd36673f6c4949528d3acce39c6395275
limcross <limcross@gmail.com>
2017-10-22 21:28:51 -0300

    change capybara driver to chrome

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,20 +9,27 @@ Combustion.initialize! :active_record, :action_controller, :action_view
 require 'rspec/rails'
 require 'capybara/rails'
 
-if ENV['CAPYBARA_DRIVER'].try(:to_sym) == :poltergeist
-  require 'capybara/poltergeist'
-  Capybara.default_driver = :poltergeist
-else
-  Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :firefox,
-      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.firefox(marionette: false)
-    )
-  end
+Capybara.register_driver :chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[window-size=1024,768] }
+  )
 
-  Capybara.default_driver = :selenium
+  Capybara::Selenium::Driver.new app, browser: :chrome,
+                                      desired_capabilities: capabilities
 end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu window-size=1024,768] }
+  )
+
+  Capybara::Selenium::Driver.new app, browser: :chrome,
+                                      desired_capabilities: capabilities
+end
+
+driver_name = (ENV['CAPYBARA_DRIVER'] || 'headless_chrome').to_sym
+Capybara.javascript_driver = driver_name
+Capybara.default_driver = driver_name
 Capybara.default_max_wait_time = 10
 
 require 'coveralls'
